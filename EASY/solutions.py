da4848491d3df8439b6e0173826a06dcf93a3126
thsmcoding <hassia.tamimou@gmail.com>
2022-12-08 18:01:40 +0100

    conversion system from base 10 to roman

--- EASY/solutions.py
+++ EASY/solutions.py
@@ -37,3 +37,23 @@ class Solutions:
             if not nbr_tostr[i] is nbr_tostr[len(nbr_tostr)-1-i]:
                 return False
         return True
+    
+    def romanToInteger(self, s):
+        symbols= {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000,
+                  'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900 }
+        numb,i=0,0
+        while (i<len(s)):
+            if s[i::i+2] in symbols and len(s[i::i+2]) == 2:
+                print(i)
+                numb+=symbols[s[i::i+2]]
+                i+=2
+            else:
+                numb+=symbols[s[i]]
+                i+=1
+        return numb 
+     
+        
+     
+        
+     
+        
