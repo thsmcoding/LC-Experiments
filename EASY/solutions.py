81113562df49f370b1dfb5ef592c7e929907d208
thsmcoding <hassia.tamimou@gmail.com>
2023-02-14 15:29:49 +0100

    API: search idnex or insert at the correct index

--- EASY/solutions.py
+++ EASY/solutions.py
@@ -154,7 +154,20 @@ class Solutions:
                  i -=1
                  length -=1
              i +=1
-         return length    
+         return length      
+        
+    def searchInsert(self, nums, target):
+        low, high=0,len(nums)
+        while(low<high):
+            mid=(high+low)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                low=mid+1
+            else:      
+                high=mid-1
+        return low
+                
     
     
         
