e249ba120b1ba5e01b87cf90a4dc26ceabc11df9
thsmcoding <hassia.tamimou@gmail.com>
2023-02-28 16:09:11 +0100

    API: compute binary version of two binary strings

--- EASY/solutions.py
+++ EASY/solutions.py
@@ -7,7 +7,7 @@ author:HTS
 
 class Solutions:    
     
-    #Finding the sum
+    #Finding the sum in an array of numbers
     def twoSum(self, nums, target):
         candidates={}
         for i in range(len(nums)):
@@ -20,3 +20,8 @@ class Solutions:
     def shorter_isPalindrome(self,nbr):
         nbr_tostr=str(nbr)
         return nbr_tostr[::] == nbr_tostr[::-1]
+    
+    #return the binary sum of two strings as a binary string
+    def addBinary(self, a, b):
+        sum_10=int(a,2)+int(b,2)
+        return str(bin(sum_10)).lstrip('-0b')
