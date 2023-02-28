c45d4889a5a8d488a4b73dc2d87622faf95fbe92
thsmcoding <hassia.tamimou@gmail.com>
2023-02-28 16:14:29 +0100

    FIX:improving docstring

--- EASY/solutions.py
+++ EASY/solutions.py
@@ -3,11 +3,9 @@
 Created on Mon Nov 28 16:02:17 2022
 author:HTS
 """
-
-
 class Solutions:    
     
-    #Finding the sum in an array of numbers
+    #twoSum: searching a target sum in an integer array
     def twoSum(self, nums, target):
         candidates={}
         for i in range(len(nums)):
@@ -16,12 +14,12 @@ class Solutions:
                 return [candidates[search], i]
             candidates[nums[i]]=i   
     
-    #isPalindrome : shorter version    
+    #shorter_isPalindrome: checking if a string is a palindrome    
     def shorter_isPalindrome(self,nbr):
         nbr_tostr=str(nbr)
         return nbr_tostr[::] == nbr_tostr[::-1]
     
-    #return the binary sum of two strings as a binary string
+    #addBinary: return the binary sum of two strings as a binary string
     def addBinary(self, a, b):
         sum_10=int(a,2)+int(b,2)
         return str(bin(sum_10)).lstrip('-0b')
